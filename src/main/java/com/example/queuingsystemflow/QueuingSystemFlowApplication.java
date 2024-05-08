5b7da1d138837be33fcd5b82898149286a2bdf99
kangseungmin <rkdtmdals999@naver.com>
2024-05-08 18:01:17 +0900

    create: Create Project

---
+++ src/main/java/com/example/queuingsystemflow/QueuingSystemFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.queuingsystemflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuingSystemFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuingSystemFlowApplication.class, args);
+	}
+
+}
