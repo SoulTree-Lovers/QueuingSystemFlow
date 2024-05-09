0d0580445156803b61b144e0eccc2c31658c3294
kangseungmin <rkdtmdals999@naver.com>
2024-05-09 17:37:29 +0900

    feat: Redis 대기열 큐에 추가하는 서비스 로직 구현

---
+++ src/main/java/com/example/queuingsystemflow/service/UserQueueService.java
@@ -0,0 +1,36 @@
+package com.example.queuingsystemflow.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
+import static com.example.queuingsystemflow.exception.ErrorCode.QUEUE_ALREADY_REGISTERED_USER;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueueService {
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+
+    // 대기열 등록 API
+    public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
+        /**
+         * redis sortedset에 저장
+         * - key: userId
+         * - value: unix timestamp
+         * - rank: 몇 번째 대기 순서인지
+         */
+        var unixTimestamp = Instant.now().getEpochSecond();
+        return reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString(), unixTimestamp)
+            .filter(i -> i)
+            .switchIfEmpty(Mono.error(QUEUE_ALREADY_REGISTERED_USER.build()))
+            .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
+            .map(i -> i >= 0 ? i+1: i)
+            ;
+
+    }
+}
