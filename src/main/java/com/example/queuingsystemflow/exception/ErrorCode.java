88937844a5a80515864df8f8499e0ad1aff8abe0
kangseungmin <rkdtmdals999@naver.com>
2024-05-11 22:53:28 +0900

    feat: Error Code Enum 구현

---
+++ src/main/java/com/example/queuingsystemflow/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.example.queuingsystemflow.exception;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public enum ErrorCode {
+    QUEUE_ALREADY_REGISTERED_USER(HttpStatus.CONFLICT, "UQ-0001", "이미 등록된 사용자입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String reason;
+
+    public ApplicationException build() {
+        return new ApplicationException(httpStatus, code, reason);
+    }
+
+    public ApplicationException build(Object ...args) {
+        return new ApplicationException(httpStatus, code, reason.formatted(args));
+    }
+}
