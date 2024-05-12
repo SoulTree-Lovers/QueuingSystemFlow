420454ddf959a837511c913341446f0400057f9e
kangseungmin <rkdtmdals999@naver.com>
2024-05-12 22:59:14 +0900

    feat: 대기열 등록 허용 메소드, 대기열 등록 가능여부 조회 메소드 구현

--- src/main/java/com/example/queuingsystemflow/service/UserQueueService.java
+++ src/main/java/com/example/queuingsystemflow/service/UserQueueService.java
@@ -14,7 +14,8 @@ import static com.example.queuingsystemflow.exception.ErrorCode.QUEUE_ALREADY_RE
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
-    private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait"; // 사용자 대기 큐
+    private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed"; // 사용자 접속 허용 큐
 
     // 대기열 등록 API
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -32,5 +33,22 @@ public class UserQueueService {
             .map(i -> i >= 0 ? i+1: i)
             ;
 
+    }
+
+    // 진입을 허용하는 메소드
+    public Mono<Long> allowUser(final String queue, final Long count) {
+        // 진입을 허용하는 단계
+        // 1. wait queue에서 사용자를 제거
+        // 2. proceed queue에 사용자를 추가
+        return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue), count) // count 개수 만큼 사용자 제거
+            .flatMap(member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
+            .count();
+    }
+
+    // 진입이 가능한 상태인지 조회
+    public Mono<Boolean> isAllowed(final String queue, final Long userId) {
+        return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue), userId.toString())
+            .defaultIfEmpty(-1L) // 값이 없다면 -1 리턴 (등록되지 않음)
+            .map(rank -> rank >= 0); // rank가 0보다 크다면 등록된 것으로 간주
     }
 }
