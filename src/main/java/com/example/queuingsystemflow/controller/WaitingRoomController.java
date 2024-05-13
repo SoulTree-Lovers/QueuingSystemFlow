dea18e65bab258645942de98eaf545ffa33a3348
kangseungmin <rkdtmdals999@naver.com>
2024-05-13 16:12:12 +0900

    feat: 대기 웹페이지 접속 허용자 리다이렉트 로직 구현 (Redirect URL은 쿼리파라미터로 받음)

--- src/main/java/com/example/queuingsystemflow/controller/WaitingRoomController.java
+++ src/main/java/com/example/queuingsystemflow/controller/WaitingRoomController.java
@@ -1,5 +1,7 @@
 package com.example.queuingsystemflow.controller;
 
+import com.example.queuingsystemflow.service.UserQueueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,15 +9,33 @@ import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
+@RequiredArgsConstructor
 public class WaitingRoomController {
+
+    private final UserQueueService userQueueService;
 
     @GetMapping("/waiting-room")
     Mono<Rendering> waitingRoomPage(
         @RequestParam(name = "queue", defaultValue = "default") String queue,
-        @RequestParam(name = "user_id") String userId
+        @RequestParam(name = "user_id") Long userId,
+        @RequestParam(name = "redirect_url") String redirectUrl
     ) {
-        // waiting-room.html 렌더링 뷰 리턴
-        return Mono.just(Rendering.view("waiting-room.html")
-            .build());
+        // 1. 입장이 허용되어 page redirect(이동)가 가능한 상태인가?
+        return userQueueService.isAllowed(queue, userId)
+            .filter(allowed -> allowed) // 입장이 허용된다면
+            // 2. 어디로 이동해야 하는가?
+            .flatMap(allowed -> Mono.just(Rendering.redirectTo(redirectUrl).build())) // 페이지 이동
+            .switchIfEmpty(
+                // 입장이 허용되지 않았다면, 대기 등록 및 웹페이지에 필요한 데이터 전달
+                userQueueService.registerWaitQueue(queue, userId)
+                    .onErrorResume(ex -> userQueueService.getRank(queue, userId)) // 등록이 되어있다면, 대기 번호 조회
+                    .map(rank -> Rendering.view("waiting-room.html")
+                        .modelAttribute("number", rank)
+                        .modelAttribute("userId", userId)
+                        .modelAttribute("queue", queue)
+                        .build()
+                )
+            );
+
     }
 }
